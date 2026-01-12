7f8a0417bc03cd4124284bce38a26bfbdda95e76
ToniAnino <63290625+ToniAnino@users.noreply.github.com>
2026-01-12 21:15:14 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/neoka-holistic-heal/',
+  base: mode === 'production' ? '/neoka-holistic-heal/' : '/',
   server: {
     host: "::",
     port: 8080,
